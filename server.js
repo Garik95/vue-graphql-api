bbc7653f4d81e90ad6a8d6111ee0795a3ee90e59
Garik Nurmurodoff <garikn95@gmail.com>
2018-07-16 11:41:20 +0500

    changed IP

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const mongoose = require('mongoose')
+const host = process.env.IP;
 const port = process.env.port;
 
 // Import GraphQL components
@@ -19,5 +20,5 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }))
 
-app.listen(port);
+app.listen(port,host);
 console.log('Running a GraphQL API server at localhost:' + port + '/graphql')
