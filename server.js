7e73d9f6ab4d27524d35cd7d0c70deeb245d1d89
Garik Nurmurodoff <garikn95@gmail.com>
2018-07-16 14:06:57 +0500

    Added CORS package

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors')
 const graphqlHTTP = require('express-graphql')
 const mongoose = require('mongoose')
 const host = process.env.IP;
@@ -14,6 +15,7 @@ mongoose.connect(dbURL + '/' + dbName)
 
 // Set up Express server
 const app = express()
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
